4a0db98670900a4e63592d2ba9b1b3aa6f696e90
akshit10-dotcom <akshitsatti@gmail.com>
2026-02-27 09:51:22 +0530

    second commit

--- script.js
+++ script.js
@@ -17,11 +17,45 @@
 //     document.write("<br/>");
 // }
 // document.write("Loop stopped");
-const person={
-    fname:"Ajay",
-    iname:"Singh",
-    age:25
-};
-for(let x in person){
-    console.log("person details:"+x+";"+person[x])
-}
+// const person={
+//     fname:"Ajay",
+//     iname:"Singh",
+//     age:25
+// };
+// for(let x in person){
+//     console.log("person details:"+x+":"+person.x)
+// }
+// +person[x] is used for accessing value or person.x
+// var count=0;
+// document.write("Starting loop");
+// while(count<10){
+//     document.write("Current Count:"+count+"<br/>");
+//     count++;
+// }
+// var age=20;
+// if(age>18){
+//     document.write("<b>You are eligible to cast the vote</b>");
+// }
+// let age=20;
+// if(age>=18){
+//     console.log("You are an adult.");
+// }
+// else{
+//     console.log("You are a minor");
+// }
+// var grade='A';
+// switch(grade){
+//     case 'A':console.log("Good job");
+//     break;
+//     case'B':console.log("Pretty good<br/>");
+//     break;
+//     default:console.log("Unknown grade<br/>")
+// }
+// function myfunction(){
+//     alert("Hello World")
+// }{
+// var res=mul(12,30);
+// function mul(x,y){
+//     return x*y;
+// }
+// console.log(res);
