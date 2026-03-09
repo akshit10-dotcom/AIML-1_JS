adc6fe1ff0646a59540b5e65b64aef1068914fd2
akshit10-dotcom <akshitsatti@gmail.com>
2026-03-09 15:50:36 +0530

    initial commit

--- script.js
+++ script.js
@@ -84,11 +84,45 @@
 // for(let x in person){
 //     console.log("person details":+x+":"+person[x])
 // }
-var person2=new Object();
-person2.name="Akshit";
-person2.age=32;
-person2.isMarried=true;
-person2.address={};
-person2.address.street="nagar road";
-person2.address.flat=33;
-console.log(person2)
+// var person2=new Object();
+// person2.name="Akshit";
+// person2.age=32;
+// person2.isMarried=true;
+// person2.address={};
+// person2.address.street="nagar road";
+// person2.address.flat=33;
+// console.log(person2)
+// Object is unordered collection of data while array is ordered
+// Array is a special type of object
+// var myinfo=new array();
+// var myinfo1=array();
+// console.log(myinfo,myinfo1);
+// let collection=[
+//     {},
+//     [],
+//     true,
+//     "john",
+//     function(){},
+//     98398288294,
+//     undefined,
+//     null,
+//     new String("abc"),
+//     new Date(),
+// ];
+// console.log(collection)
+// collection.teacherName = "John Max";
+// collection.phoneNo = 4732075984;
+// console.log(collection)
+// var trainerinfo=["Akshit",18,"Ghaziabad",{isMarried:false}];
+// console.log(trainerinfo[trainerinfo.length-1]);
+// DOM document oject model it enables  js  to  dyamically access manipulate and update content
+// Window object is object of the browser which is always at the top of the hierarchy
+// Document object - if there is a need to access any element in an html page, we always start with accessing the document object
+// Document object is the property of the window object
+// DOM manipulation in js
+// const result = document.getElementById("para");
+// console.log(result);
+// result.innerHTML = "my self";
+// result.title = "myname";
+const result2 = document.getElementsByClassName("myClass");
+result2.innerHTML = "hii";
